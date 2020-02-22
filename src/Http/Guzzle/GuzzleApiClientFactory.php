0d8ed5550a9b453436d74e7296019594afb99d06
Nick Veenhof <nickveenhof@Nicks-MacBook-Pro.local>
2020-02-22 13:22:41 +0100

    Moved classes that belong to unomi to the sdk

--- src/Http/Guzzle/GuzzleApiClientFactory.php
+++ src/Http/Guzzle/GuzzleApiClientFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Dropsolid\UnomiSdkPhp\Client\Http\Guzzle;
+namespace Dropsolid\UnomiSdkPhp\Http\Guzzle;
 
 use Http\Adapter\Guzzle6\Client;
 use Dropsolid\UnomiSdkPhp\Http\ApiClient\ApiClient;
@@ -34,4 +34,59 @@ class GuzzleApiClientFactory
 
         return new ApiClient(null, $psrClient, null,  $config);
     }
+
+    /**
+   * @param AbstractProvider $provider
+   * @param AccessToken|array $accessToken
+   * @param array $config
+   * @return ApiClient
+   */
+  public static function createOAuth(
+    AbstractProvider $provider,
+    AccessToken $accessToken,
+    array $config = []
+  ) {
+    if (!isset($config['handler'])) {
+      if (!isset($config['callback'])) {
+        $config['callback'] = null;
+      }
+      $handler = self::initialiseHandlerStack(
+        $provider,
+        $accessToken,
+        $config['callback']
+      );
+
+      $config['handler'] = $handler;
+    }
+    if (!isset($config['options'])) {
+      $config['options'] = [];
+    }
+
+    $httpClient = new GuzzleHttpClient($config);
+    $psrClient = new Client($httpClient);
+
+    return new ApiClient($provider, $psrClient, $accessToken, $config);
+  }
+
+  /**
+   * @param AbstractProvider $provider
+   * @param AccessToken $accessToken
+   * @param callable $refreshTokenCallback
+   * @return HandlerStack
+   */
+  protected static function initialiseHandlerStack(
+    AbstractProvider $provider,
+    AccessToken $accessToken,
+    callable $refreshTokenCallback = null
+  ) {
+    $handlerStack = HandlerStack::create();
+    $refreshMiddleware = new RefreshTokenMiddleware(
+      $provider,
+      $accessToken,
+      $refreshTokenCallback
+    );
+    $handlerStack->push($refreshMiddleware);
+
+    return $handlerStack;
+  }
 }
